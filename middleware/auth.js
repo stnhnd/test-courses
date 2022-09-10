bdee0083aa2027a5c3af1c018d006e73aefa0887
Minimal <minimal.wbd@gmail.com>
2022-09-10 09:04:26 +0300

    clear comments

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,4 @@ export default function ({ $axios, store, route, redirect }) {
   if (!store.state.authed && route.name != 'index') {
     return redirect('/')
   }
-  // if (route.name == 'index') {
-  //   return redirect('/courses/page/1')
-  // }
 }
