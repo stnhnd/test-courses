1af364bfd4ece369f8930b677197597a01f9ceda
Minimal <minimal.wbd@gmail.com>
2022-09-10 08:37:40 +0300

    404 error

---
+++ layouts/error.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="flex flex-col align-center pt-28 pb-28">
+    <h1 class="text-6xl text-gray-700 mb-4 text-center font-medium">Error 404</h1>
+    <p class="flex self-center">Page not found</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
